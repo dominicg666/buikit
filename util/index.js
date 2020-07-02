6470cd459bcf9b027bf042564d02406092f56068
Dominic <dominic.webandcrafts@gmail.com>
2020-07-02 22:23:19 +0530

    add initial compoents

---
+++ util/index.js
@@ -0,0 +1,13 @@
+export { default as getDisplayName } from './getDisplayName';
+export { default as makeUrl } from './makeUrl';
+
+export {
+    VALID_SERVICE_WORKER_ENVIRONMENT,
+    registerMessageHandler,
+    unRegisterMessageHandler,
+    handleMessageFromSW,
+    sendMessageToSW
+} from './swUtils';
+
+
+
